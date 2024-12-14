f969c0c0fe2b7afc91980881dc3d6015742b8da1
Vegirskiy_Lord <dmitrijrodikovbvb@gmail.com>
2024-12-14 21:46:34 +0300

    UPDATE 0.1 - Я опять поел

--- Program.cs
+++ Program.cs
@@ -22,9 +22,33 @@ namespace classnasledovaniye2
             partTimeEmployee.Salary = 1820;
             partTimeEmployee.DisplayInfo();
 
-
             Console.WriteLine(account.Deposit(1820, 1.5, 365));
             account.Withdraw();
+
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            Console.WriteLine("Кот говорит: "+ cat.Speak());
+            Console.WriteLine("Собака говорит: " + dog.Speak());
+
+            EmailTask emailTask = new EmailTask();
+            DatabaseTask databaseTask = new DatabaseTask();
+            emailTask.Execute();
+            databaseTask.Execute();
+
+            UserAdmin userAdmin = new UserAdmin();
+            RegularUser admin = new RegularUser();
+            userAdmin.Login();
+            admin.Login();
+
+            Circle circle = new Circle();
+            Square square = new Square();
+            Console.WriteLine(circle.GetInfo());
+            Console.WriteLine(square.GetInfo());
+
+            House house = new House();
+            ApartmentBuilding apartmentBuilding = new ApartmentBuilding();
+            OfficeBuilding officeBuilding = new OfficeBuilding();
+
 
             Console.ReadKey();
 
